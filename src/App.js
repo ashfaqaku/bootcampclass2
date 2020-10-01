25ee99edcd05e4bc5d74dc4771b826b393dbcb28
Muhammad Ashfaq <ashfaqaku@gmail.com>
2020-10-01 21:03:25 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          hello ahsfaq this is ci/cd project
+          hello ashfaq this is ci/cd project
         </a>
       </header>
     </div>
