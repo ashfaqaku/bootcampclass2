f1867473d1bed273dafdbe40e510bfea9427e6a7
Muhammad Ashfaq <ashfaqaku@gmail.com>
2020-10-01 21:00:27 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Rust
+          hello ahsfaq this is ci/cd project
         </a>
       </header>
     </div>
